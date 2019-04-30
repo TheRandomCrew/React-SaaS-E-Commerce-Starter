5553aa9d72053d34135053e7aefec4acaa698d34
Israel-Laguan <israellaguan@gmail.com>
2019-04-30 15:32:46 -0500

    Added Sidebar

--- app/src/index.js
+++ app/src/index.js
@@ -2,46 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'typeface-clear-sans';
 import * as serviceWorker from './util/serviceWorker';
-import { Grommet } from 'grommet';
 import AppRouter from './router/router';
 
-const theme = {
-    global: {
-      colors: {
-        'light-2': '#f5f5f5',
-        'text': {
-          'light': 'rgba(0, 0, 0, 0.87)',
-        },
-        'primary': '#777',
-      },
-      edgeSize: {
-        small: '14px',
-      },
-      elevation: {
-        light: {
-          medium: '0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12)',
-        },
-      },
-      font: {
-        size: '14px',
-        height: '20px',
-      },
-    },
-    button: {
-      border: {
-        width: '1px',
-        radius: '4px',
-      },
-      padding: {
-        vertical: '8px',
-        horizontal: '16px',
-      }
-    },
-  };
-
-ReactDOM.render(<Grommet theme={theme} full>
-    <AppRouter />
-</Grommet>, document.getElementById('root'));
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- app/src/page/Home.jsx
+++ app/src/page/Home.jsx
@@ -1,32 +1,10 @@
 import React from 'react'
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { 
-    faShareAlt,
-    faSearch, 
-    faLink, 
-    faExternalLinkAlt, 
-    faBlind,
-    faAlignLeft,
-    faListOl,
-    faChartBar,
-    faExclamationTriangle,
-    faLightbulb 
-} from '@fortawesome/free-solid-svg-icons';
-library.add(faSearch);
-library.add(faShareAlt);
-library.add(faLink );
-library.add(faExternalLinkAlt);
-library.add(faBlind);
-library.add(faListOl);
-library.add(faChartBar);
-library.add(faAlignLeft);
-library.add(faLightbulb);
-library.add(faExclamationTriangle);
-    
+import { Cycle } from 'grommet-icons';
 const Home = ({user}) => (
     <div>
         <h1>Component</h1>
         {user&& <p>{user.given_name}</p>}
+        <Cycle/>
     </div>
 )
 
